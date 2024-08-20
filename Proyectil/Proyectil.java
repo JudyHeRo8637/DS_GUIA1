df3a0d9c645926d7d2a079b92dd2cdeb79716d1d
JudyHeRo8637 <jheredia8637@universidadean.edu.co>
2024-08-19 23:42:33 -0500

    Add files via upload

---
+++ Proyectil/Proyectil.java
@@ -0,0 +1,74 @@
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class Proyectil {
+
+    /**
+     * Default constructor
+     */
+    public Proyectil() {
+    }
+
+    /**
+     * 
+     */
+    private float Velocidad;
+
+    /**
+     * 
+     */
+    private float Angulo;
+
+    /**
+     * @return
+     */
+    public float Calcular Altura Maxima() {
+        // TODO implement here
+        return 0.0f;
+    }
+
+    /**
+     * @return
+     */
+    public float Calcular Tiempo Vuelo() {
+        // TODO implement here
+        return 0.0f;
+    }
+
+    /**
+     * @return
+     */
+    public float CalcularTrayectoria() {
+        // TODO implement here
+        return 0.0f;
+    }
+
+    /**
+     * @return
+     */
+    public float Calcular Velocidad() {
+        // TODO implement here
+        return 0.0f;
+    }
+
+    /**
+     * @return
+     */
+    public float Actualizar Velocidad() {
+        // TODO implement here
+        return 0.0f;
+    }
+
+    /**
+     * @return
+     */
+    public float Actualizar Angulo() {
+        // TODO implement here
+        return 0.0f;
+    }
+
+}
