161a2c5e73516d341e8ae8cd0c97239628c6c0ad
JudyHeRo8637 <jheredia8637@universidadean.edu.co>
2024-08-19 23:42:18 -0500

    Add files via upload

---
+++ Fecha/Fecha.java
@@ -0,0 +1,76 @@
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class Fecha {
+
+    /**
+     * Default constructor
+     */
+    public Fecha() {
+    }
+
+    /**
+     * 
+     */
+    private int Año;
+
+    /**
+     * 
+     */
+    private int Mes;
+
+    /**
+     * 
+     */
+    private int Día;
+
+    /**
+     * @return
+     */
+    public int Establecer Año() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @return
+     */
+    public int Establecer Mes() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @return
+     */
+    public int Establecer Día() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @param Año
+     */
+    public void Obtener Año(int Año) {
+        // TODO implement here
+    }
+
+    /**
+     * @param Mes
+     */
+    public void Obtener Mes(int Mes) {
+        // TODO implement here
+    }
+
+    /**
+     * @param Día
+     */
+    public void Obtener Día(int Día) {
+        // TODO implement here
+    }
+
+}
