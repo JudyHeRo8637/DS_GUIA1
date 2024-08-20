48f842846d1ff75879a0b63e1d86f23555572994
JudyHeRo8637 <jheredia8637@universidadean.edu.co>
2024-08-19 23:41:55 -0500

    Add files via upload

---
+++ Fraccionario/Fraccionario.java
@@ -0,0 +1,66 @@
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class Fraccionario {
+
+    /**
+     * Default constructor
+     */
+    public Fraccionario() {
+    }
+
+    /**
+     * 
+     */
+    public int Numerados;
+
+    /**
+     * 
+     */
+    public int denominador;
+
+    /**
+     * 
+     */
+    public void Attribute1;
+
+    /**
+     * 
+     */
+    public void Sumar() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Restar() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Multiplicar() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Dividir() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Simplificar() {
+        // TODO implement here
+    }
+
+}
